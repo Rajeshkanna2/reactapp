81e6af9848e27fa97d2e522008f575ef35b56ecd
Rajeshkanna2 <rajeshkannanb2@gmail.com>
2024-01-22 17:55:02 +0530

    added

--- home/Home.jsx
+++ home/Home.jsx
@@ -2,35 +2,35 @@ import React from 'react'
 import Growingplant from '../growing plants/Growingplant'
 
 function Home() {
-    const plantdetails ={
+    const plantdetails = {
         name: 'Rose',
         icon: '🌹',
         size: 30,
         color: 'pinkred',
-       }
+    }
 
-       const plantdetails2 ={
+    const plantdetails2 = {
         name: 'sunflower',
         icon: '🌼',
         size: 40,
         color: 'white',
-       }
-    const plantdetails3={
+    }
+    const plantdetails3 = {
         name: 'Grass',
         icon: '🌱',
         size: 10,
         color: 'Green',
-    } 
+    }
     return (
         <div>
-           <center> <h1>Welcome to Home</h1> </center>
-           <hr></hr>
-    <Growingplant plantdetails={plantdetails2}/> 
-    <hr></hr>
-    <Growingplant plantdetails= {plantdetails} />
-    <Growingplant/>
-            <Growingplant plantdetails={plantdetails3}/>>
-    
+            <center> <h1>Welcome to Home</h1> </center>
+            <hr></hr>
+            <Growingplant plantdetails={plantdetails2} />
+            <hr></hr>
+            <Growingplant plantdetails={plantdetails} />
+            <Growingplant />
+            <Growingplant plantdetails={plantdetails3} />
+
         </div>
     )
 }
